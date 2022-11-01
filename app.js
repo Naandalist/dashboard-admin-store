34889f1b63f10d1e8e03b050d0168dd8822afe4f
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-01 19:04:58 +0700

    feat: add methodOverride middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const methodOverride = require('method-override')
 
 var dashboardRouter = require("./app/dashboard/router");
 var categoryRouter = require("./app/category/router");
@@ -14,6 +15,8 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('X-HTTP-Method-Override'))
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
