85bf29b300f7529c9582fa146baaafd3077b5b34
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-01 20:59:13 +0700

    feat: set connect flash express session

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 var dashboardRouter = require("./app/dashboard/router");
 var categoryRouter = require("./app/category/router");
-
 
 var app = express();
 
@@ -15,8 +16,19 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+);
+
+app.use(flash());
+
 // override with the X-HTTP-Method-Override header in the request
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,7 +42,6 @@ app.use(
 
 app.use("/", dashboardRouter);
 app.use("/category", categoryRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
