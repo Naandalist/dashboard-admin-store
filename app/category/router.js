4555f0eb96afe27949b8963bc2668e79a502d91f
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-01 19:23:04 +0700

    feat: add  router update category

--- app/category/router.js
+++ app/category/router.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { index, viewCreate, actionCreate } = require("./controller");
+const {
+  index,
+  viewCreate,
+  actionCreate,
+  viewUpdate,
+  actionUpdate,
+} = require("./controller");
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
+router.get("/update/:id", viewUpdate);
+router.put("/update/:id", actionUpdate);
 
 module.exports = router;
