526224da2c84f82009ee6d2d3a1efb36e217221c
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-02 13:56:50 +0700

    feat: add router nominal

--- app/nominal/router.js
+++ app/nominal/router.js
@@ -5,16 +5,16 @@ const {
   index,
   viewCreate,
   actionCreate,
-//   viewUpdate,
-//   actionUpdate,
-//   actionDelete,
+  viewUpdate,
+  actionUpdate,
+  actionDelete,
 } = require("./controller");
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
-// router.get("/update/:id", viewUpdate);
-// router.put("/update/:id", actionUpdate);
-// router.delete("/delete/:id", actionDelete);
+router.get("/update/:id", viewUpdate);
+router.put("/update/:id", actionUpdate);
+router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
