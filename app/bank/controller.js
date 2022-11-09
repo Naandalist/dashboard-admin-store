ff13a658eab8ff5b7df0e03fc99510cf3f3b3766
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-09 13:21:40 +0700

    feat: add controller for bank

---
+++ app/bank/controller.js
@@ -0,0 +1,101 @@
+const Bank = require("./model");
+
+module.exports = {
+  index: async (req, res) => {
+    const alertMessage = req.flash("alertMessage");
+    const alertStatus = req.flash("alertStatus");
+
+    const alert = { message: alertMessage, status: alertStatus };
+
+    const bank = await Bank.find();
+    console.log("bank: ", bank);
+    try {
+      res.render("admin/bank/view_bank", {
+        bank,
+        alert,
+      });
+    } catch (err) {
+      req.flash("alertMessage", err.message);
+      req.flash("alert status", "danger");
+      res.redirect("/bank");
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/bank/create", {});
+    } catch (err) {
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { ownerName, bankName, accountNumber } = req.body;
+      let bank = await Bank({ ownerName, bankName, accountNumber });
+      await bank.save();
+
+      req.flash("alertMessage", "add new bank is successfull");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/bank");
+    } catch (err) {
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+  viewUpdate: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+
+      res.render("admin/bank/update", {
+        bank,
+      });
+    } catch (err) {
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+  actionUpdate: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { ownerName, bankName, accountNumber } = req.body;
+
+      const bank = await Bank.findOneAndUpdate(
+        { _id: id },
+        { ownerName, bankName, accountNumber }
+      );
+
+      req.flash("alertMessage", `Update ${ownerName}'s bank is successfull`);
+      req.flash("alertStatus", "success");
+      res.redirect("/bank");
+    } catch (err) {
+      req.flash("alertMessage", err.message);
+      req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    }
+  },
+
+    actionDelete: async (req, res) => {
+      try {
+        const { id } = req.params;
+
+        const bank = await Bank.findOneAndRemove({ _id: id });
+
+        req.flash(
+          "alertMessage",
+          `Bank with id ${id} is deleted successfully`
+        );
+        req.flash("alertStatus", "success");
+
+        res.redirect("/bank");
+      } catch (err) {
+        req.flash("alertMessage", err.message);
+        req.flash("alertStatus", "danger");
+        res.redirect("/bank");
+      }
+    },
+};
