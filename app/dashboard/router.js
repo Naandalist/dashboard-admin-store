3b9eae833ccb146f601f2e87d56a031d43a94c6c
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-12 10:07:12 +0700

    feat: add auth for dashboard

--- app/dashboard/router.js
+++ app/dashboard/router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const { isLoginAdmin } = require("../middleware/auth");
 const { index } = require("./controller");
 
+router.use(isLoginAdmin);
 router.get("/", index);
 
 module.exports = router;
