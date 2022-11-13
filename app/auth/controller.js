eec333a48f449a845721fda8b71b48463901b827
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-14 01:53:58 +0700

    feat: add controller authenticate

---
+++ app/auth/controller.js
@@ -0,0 +1,33 @@
+const Player = require("../player/model");
+
+module.exports = {
+  signUp: async (req, res, next) => {
+    try {
+      //   const { name, email, username, password } = req.body;
+      const payload = req.body;
+
+      if (req.file) {
+      } else {
+        const player = new Player(payload);
+        await player.save();
+
+        delete player._doc.password;
+
+        res.status(201).json({
+          data: player,
+        });
+      }
+
+      res.status(201).json({
+        message: payload,
+      });
+    } catch (err) {
+      if (err && err.name === "ValidationError") {
+        return res
+          .status(422)
+          .json({ error: 1, message: err.message, fields: err.error });
+      }
+      next(err);
+    }
+  },
+};
