5107f8e21f14cea044d9a40406fb2ddfc592e5d5
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-14 01:12:48 +0700

    feat: Create api detail voucher

--- app/player/controller.js
+++ app/player/controller.js
@@ -4,7 +4,32 @@ const Voucher = require("../voucher/model");
 module.exports = {
   landingPage: async (req, res) => {
     try {
-      const voucher = await Voucher.find().populate("category");
+      const voucher = await Voucher.find()
+        .select("_id name status category thumbnail")
+        .populate("category");
+
+      res.status(200).json({
+        data: voucher,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals")
+        .populate("user", "_id name phoneNumber");
+
+      console.log("data voucher: ", voucher);
+
+      if (!voucher) {
+        return res.status(404).json({ message: "Data is not found.!" });
+      }
 
       res.status(200).json({
         data: voucher,
