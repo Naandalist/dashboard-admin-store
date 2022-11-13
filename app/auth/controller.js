1a9ec506adcd6da48fe4f94586c14276c8401f3d
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-14 02:37:38 +0700

    feat: handle sign up with or without avatar

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,5 +1,9 @@
 const Player = require("../player/model");
 
+const fs = require("fs");
+const path = require("path");
+const config = require("../../config");
+
 module.exports = {
   signUp: async (req, res, next) => {
     try {
@@ -7,6 +11,49 @@ module.exports = {
       const payload = req.body;
 
       if (req.file) {
+        let tempPath = req.file.path;
+
+        let originalExt =
+          req.file.originalname.split(".")[
+            req.file.originalname.split(".").length - 1
+          ];
+
+        let fileName = req.file.filename + "." + originalExt;
+
+        let targetPath = path.resolve(
+          config.rootPath,
+          `public/uploads/${fileName}`
+        );
+
+        const src = fs.createReadStream(tempPath);
+        const dest = fs.createWriteStream(targetPath);
+
+        src.pipe(dest);
+
+        src.on("end", async () => {
+          try {
+            const player = new Player({
+              ...payload,
+              avatar: fileName,
+            });
+
+            await player.save();
+
+            //delete original password string, because we just save only the hashed
+            delete player._doc.password;
+
+            res.status(201).json({
+              data: player,
+            });
+          } catch (err) {
+            if (err && err.name === "ValidationError") {
+              return res
+                .status(422)
+                .json({ error: 1, message: err.message, fields: err.errors });
+            }
+            next(err);
+          }
+        });
       } else {
         const player = new Player(payload);
         await player.save();
@@ -17,10 +64,6 @@ module.exports = {
           data: player,
         });
       }
-
-      res.status(201).json({
-        message: payload,
-      });
     } catch (err) {
       if (err && err.name === "ValidationError") {
         return res
