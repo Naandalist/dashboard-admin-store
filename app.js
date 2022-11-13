b11c2c8f6111e9cb99e449b60287c986b1b6f7c0
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-13 11:09:00 +0700

    feat: registered api routing

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ const bankRouter = require("./app/bank/router");
 const paymentRouter = require("./app/payment/router");
 const userRouter = require("./app/users/router");
 const transactionRouter = require("./app/transaction/router");
+const playerRouter = require("./app/player/router");
 
 const app = express();
+const apiUrl = `/api/v1`;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -53,6 +55,9 @@ app.use("/voucher", voucherRouter);
 app.use("/bank", bankRouter);
 app.use("/payment", paymentRouter);
 app.use("/transaction", transactionRouter);
+
+// API
+app.use(`${apiUrl}/players`, playerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
