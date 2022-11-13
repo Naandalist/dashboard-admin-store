e0ccc651cbb311e030d267108def96ba5f19ca22
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-13 10:05:42 +0700

    feat: add info count for dashboard

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,10 +1,27 @@
+const Category = require("../category/model");
+const Player = require("../player/model");
+const Transaction = require("../transaction/model");
+const Voucher = require("../voucher/model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      const {name } =req.session.user
-      res.render("index", {
+      const { name } = req.session.user;
+
+      const transaction = await Transaction.countDocuments();
+      const voucher = await Voucher.countDocuments();
+      const category = await Category.countDocuments();
+      const player = await Player.countDocuments();
+
+      res.render("admin/dashboard/view_dashboard", {
         name,
-        title: "Dashboard"
+        title: "Dashboard",
+        count: {
+          transaction,
+          voucher,
+          category,
+          player,
+        },
       });
     } catch (error) {
       console.log("error: ", error);
