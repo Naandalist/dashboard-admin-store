4815645af67ec606cb5ceb532ec71fe52394f4db
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-14 14:01:08 +0700

    feat: add signin auth router

--- app/auth/router.js
+++ app/auth/router.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const os = require("os");
-const { signUp } = require("./controller");
+const { signUp, signIn } = require("./controller");
 
 router.post("/signup", multer({ dest: os.tmpdir() }).single("avatar"), signUp);
+router.post("/signin", signIn);
 
 module.exports = router;
