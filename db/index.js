bd91652fd87b5003cbfaee8c330ea34b414fd38f
“naandalist” <listiananda.apriliawan@gmail.com>
2022-11-15 22:22:55 +0700

    chore: create marker for DB connect

--- db/index.js
+++ db/index.js
@@ -1,13 +1,23 @@
 const monggose = require("mongoose");
-
 const { urlDb } = require("../config");
 
-monggose.connect(urlDb, {
-//   useUnifiedTopology: true,
-//   useFindAndModify: true,
-//   useCreateIndex: true,
-});
-
+monggose.connect(
+  urlDb,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  async function (req, res) {
+    try {
+      await console.log("Connected to Database");
+      console.log("ready state >>> ", monggose.connection.readyState);
+    } catch (err) {
+      throw err;
+    }
+  }
+);
+Í
 const db = monggose.connection;
 
 module.exports = db;
+
